b9f4fc311eeb04de753f5e6dc14df1c9112771d6
ssshhh0402 <ssshhh0402@naver.com>
2019-11-04 19:55:04 +0900

    20191104

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Subject from './components/Subject';
+import TOC from './components/TOC';
+import Content from './components/Content';
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      mode: 'welcome',
+      subject:{title : 'WEB' , sub : 'World Wide Web!'},
+      welcome:{title:'Welcome', desc: 'Hello, React'},
+      contents : [
+        {id:1, title: 'HTML', desc : 'HTML is HyperText ...'},
+        {id:2, title: 'CSS', desc : 'CSS is for design'},
+        {id:3, title: 'JavaScript', desc: 'JavaScript is for interactive'}
+      ]
+    }
+  }
+  render(){
+    return (
+      <div className = 'App'>
+        <Subject
+        title = {this.state.subject.title}
+        sub = {this.state.subject.sub} 
+        />
+        <TOC data = {this.state.contents}></TOC>
+        <Content title='HTML' desc = 'HTML is HyperText Markup Language.'></Content>
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/components/TOC.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+class TOC extends Component {
+    render(){
+        var lists = [];
+        var data = this.props.data;
+        var i = 0;
+        while(i < data.length){
+            lists.push(<li key={data[i].id}>{data[i].title}</li>);
+            i = i + 1;
+        }
+        return (
+            <nav>
+                <ul>
+                    {lists}
+                </ul>
+            </nav>
+        );
+    }
+}
+export default TOC;
