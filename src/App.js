12457c236872df50700f5db40129e4dd93df4d72
ssshhh0402 <ssshhh0402@naver.com>
2019-11-06 19:59:40 +0900

    React-17_1~3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React,{Component} from 'react';
 import './App.css';
-//import Subject from './components/Subject';
+import Subject from './components/Subject';
 import TOC from './components/TOC';
 import Content from './components/Content';
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      mode: 'read',
+      selected_content_id : 1,
+      mode: 'welcome',
       subject:{title : 'WEB' , sub : 'World Wide Web!'},
       welcome:{title:'Welcome', desc: 'Hello, React'},
       contents : [
@@ -24,32 +25,48 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     }else if(this.state.mode ==='read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i += 1
+      }
     }
     return (
       <div className = 'App'>
-        {/* <Subject
+        <Subject
         title = {this.state.subject.title}
-        sub = {this.state.subject.sub} 
-        /> */}
-        <header>
-            <h1>
-              <a href="/" onClick={function(e){
-                  e.preventDefault();
-                  if (this.state.mode === 'welcome'){
-                    this.setState({
-                      mode : 'read'
-                    })
-                  }else{
-                  this.setState({
-                    mode : 'welcome'
-                  })
-                }  
-             }.bind(this)}>{this.state.subject.title}</a></h1>
-            {this.state.subject.sub}
-        </header>
-        <TOC data = {this.state.contents}></TOC>
+        sub = {this.state.subject.sub}
+        onChangePage={function(){
+          if (this.state.mode === 'welcome'){
+            this.setState({
+              mode : 'read',
+            })
+          }else{
+          this.setState({
+            mode : 'welcome'
+          })
+        }  
+        }.bind(this)}  
+        />
+     
+        <TOC onChangePage={function(id){
+               if (this.state.mode === 'welcome'){
+                this.setState({
+                  mode : 'read',
+                  selected_content_id : Number(id)
+                })
+              }else{
+              this.setState({
+                selected_content_id : Number(id)
+              })
+            }  
+            }.bind(this)
+        }  data = {this.state.contents}></TOC>
         <Content title={_title} desc ={_desc}></Content>
       </div>
     );
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -5,7 +5,16 @@ class TOC extends Component {
         var data = this.props.data;
         var i = 0;
         while(i < data.length){
-            lists.push(<li key={data[i].id}>{data[i].title}</li>);
+            lists.push(
+            <li key={data[i].id} data-id = {data[i].id} onClick={function(e){
+                this.props.onChangePage(e.target.dataset.id);
+            }.bind(this)}>
+            {/* <li key={data[i].id} onClick={function(id,e){
+                this.props.onChangePage(id);
+            }.bind(this,data[i].id)} bind에 두번쨰 인자를 주면 그 인자가 function안의 첫번째 인자로 들어간다
+            bind읭 뒤에서부터 function의 앞으로 들어간다. */}
+                {data[i].title}
+                </li>);
             i = i + 1;
         }
         return (
